097856e9617232cd64129100b2144ddf56dc3be6
unknown <ashishjoshi1623@gmail.com>
2024-05-17 16:41:57 +0530

    Add password generator and encrypter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,55 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import bcrypt from 'bcryptjs';
+
 
 function App() {
   const [password,setPassword] = useState("");
-  const [length, setLength] = useState(18);
+  const [length, setLength] = useState(8);
   const [charAllowed, setCharAllowed] = useState(false);
-  const [symbolAllowed, setSymbolAllowed] = useState(false);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [encPass,setEncPass] = useState("");
+
+  const passRef = useRef(null);
+  const encryptPassword = useRef(null);
+  const encpass = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-    if(charAllowed) str += "0123456789"
-    if(symbolAllowed) str += "!@#$%^&*-_+=[]{}~`"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    for(let i=0; i<length; i++){
+    for(let i=1; i<=length; i++){
       let charidx = Math.floor(Math.random() * str.length + 1);
-      console.log(charidx);
+      pass += str.charAt(charidx);
     }
-  },[length,charAllowed,symbolAllowed,password])
+    setPassword(pass);
+  },[length,charAllowed,numberAllowed,setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passRef.current?.select();
+    passRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  },[password])
+
+  const copyEncryptPasswordToClipboard = useCallback(() => {
+    encpass.current?.select();
+    window.navigator.clipboard.writeText(encPass);
+  },[encPass])
+
+  const passEncrypt = async() => {
+    let pass = encryptPassword.current.value;
+    const hash = await bcrypt.hash(pass,10);
+    setEncPass(hash);
+  }
+
+  useEffect(() => {
+    passwordGenerator()
+  },[length,charAllowed,numberAllowed,setPassword])
 
   return (
     <>
@@ -29,38 +58,60 @@ function App() {
           <h1 className="heading-text">Password Generator</h1>
         </div>
         <div className="input-btn mb-3">
-          <input type="text" className="password mx-1 rounded p-1" placeholder='Password' value={password}/>
-          <button className='btn btn-primary m-1'>copy</button>
+          <input type="text" 
+          className="password mx-1 rounded p-1 input" 
+          value={password}
+          placeholder='password'
+          readOnly
+          ref={passRef}
+          />
+          <button className='btn btn-primary m-1' onClick={copyPasswordToClipboard}>copy</button>
         </div>
+
         <div className="input-check">
 
-          <input type="range" 
+          <input 
+          type="range" 
           min={6}
-          max={99}
+          max={100}
           value={length}
-          onChange={(e) => setLength(e.target.value)}
+          onChange={(e) => {setLength(e.target.value)}}
           />
           <label className='mx-1' htmlFor="length">Length : {length}</label>
 
           <input type="checkbox" 
           name="charallowed" 
           id="charallow" 
-          value={charAllowed}
-          onChange={() => setCharAllowed( (prev) => !prev)}
+          defaultChecked={numberAllowed}
+          onChange={() => {setNumberAllowed( (prev) => !prev)}}
           className='mx-2'
           />
-          <label htmlFor="Char">Character</label>
+          <label htmlFor="Char">Numbers</label>
 
           <input type="checkbox" 
-          name="symbolallowed" 
-          id="symbolallow" 
-          value={symbolAllowed}
-          onChange={() => setSymbolAllowed( (prev) => !prev)}
+          id="charallow" 
+          defaultChecked={charAllowed}
+          onChange={() => {setCharAllowed( (prev) => !prev)}}
           className='mx-2'
           />
           <label htmlFor="Char">Symbols</label>
         </div>
-        <input type="text" className='mt-3 p-1 rounded-2' placeholder='Paste your password here'/>
+
+        {/* encryption */}
+        <div>
+        <input 
+        type="text" 
+        className='mt-3 p-1 rounded-2 input' 
+        placeholder='Paste your password here'
+        ref={encryptPassword}
+        />
+        <button style={{border : '1px solid aqua'}}className='btn btn-dark mx-1' onClick={passEncrypt}>Get Encrypted</button>
+        </div>
+        <div>
+        <input type="text" className='rounded m-3 p-3 input' ref={encpass} defaultValue={encPass} readOnly placeholder='Encrypted password appear here'/>
+        <button className='btn btn-primary m-1' onClick={copyEncryptPasswordToClipboard}>copy</button>
+        </div>
+        
       </section>
     </>
   )
